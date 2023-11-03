155f1adea121d359ab74170a6361075138173bb7
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-11-03 15:30:29 +0000

    Use wasm-tools in test

--- test/WasmComponentSdkTest/WasmComponentSdkTest/SimpleProducerConsumerTest.cs
+++ test/WasmComponentSdkTest/WasmComponentSdkTest/SimpleProducerConsumerTest.cs
@@ -1,12 +1,57 @@
+using System.Diagnostics;
+using System.Reflection;
 using Xunit;
 
 namespace WasmComponentSdkTest;
 
 public class SimpleProducerConsumerTest
 {
-    [Fact]
-    public void Test1()
-    {
+    // Unfortunately it doesn't seem possible to use wasmtime-dotnet with the component model yet,
+    // (there's literally no mention of the entire concept within the wasmtime-dotnet repo), so for
+    // now the tests work by invoking the wasmtime CLI
 
+    [Fact]
+    public void CanBuildComponentWithImport()
+    {
+        var witInfo = GetWitInfo(FindModulePath("../testapps/SimpleConsumer", "SimpleConsumer.component.wasm"));
+
+        Assert.Contains("import test:producer-consumer/operations", witInfo);
+    }
+
+    [Fact]
+    public void CanBuildComponentWithExport()
+    {
+        var witInfo = GetWitInfo(FindModulePath("../testapps/SimpleProducer", "SimpleProducer.component.wasm"));
+        Assert.Contains("export test:producer-consumer/operations", witInfo);
+    }
+
+    private static string GetWitInfo(string componentFilePath)
+    {
+        var startInfo = new ProcessStartInfo(WasmToolsExePath, $"component wit {componentFilePath}") { RedirectStandardOutput = true };
+        var witInfo = Process.Start(startInfo)!.StandardOutput.ReadToEnd();
+        return witInfo;
+    }
+
+    private static string WasmtimeExePath { get; } = GetAssemblyMetadataValue("WasmtimeExe")!;
+    private static string WasmToolsExePath { get; } = GetAssemblyMetadataValue("WasmToolsExe")!;
+
+    private static string? GetAssemblyMetadataValue(string key) =>
+        typeof(SimpleProducerConsumerTest).Assembly
+        .GetCustomAttributes<AssemblyMetadataAttribute>()
+        .SingleOrDefault(x => x.Key == key)?.Value;
+
+    private static string FindModulePath(string searchDir, string filename)
+    {
+        var resolvedSearchDir = Path.Combine(
+            Path.GetDirectoryName(typeof(SimpleProducerConsumerTest).Assembly.Location)!,
+            "../../..",
+            searchDir);
+        if (!Directory.Exists(resolvedSearchDir))
+        {
+            throw new InvalidOperationException($"No such directory: {Path.GetFullPath(resolvedSearchDir)}");
+        }
+
+        var matches = Directory.GetFiles(resolvedSearchDir, filename, SearchOption.AllDirectories);
+        return Path.GetFullPath(matches.Single());
     }
 }
