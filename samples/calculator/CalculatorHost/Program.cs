0c10e0262b1741131c39bfd279a3caec9a335c33
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-11-03 09:36:28 +0000

    Add corresponding calculator host sample

---
+++ samples/calculator/CalculatorHost/Program.cs
@@ -0,0 +1,8 @@
+﻿using wit_hostapp.Wit.imports.example.calculator.Operations;
+
+Console.WriteLine("Hello, World!");
+
+var left = 123;
+var right = 456;
+var result = OperationsInterop.Add(left, right);
+Console.WriteLine($"{left} + {right} = {result}");
