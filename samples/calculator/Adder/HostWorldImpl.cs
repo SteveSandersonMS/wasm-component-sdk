21e4f357d4198feb27778ff0c6bc727c196a2470
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-11-03 09:30:23 +0000

    Make the Adder example valid

--- samples/calculator/Adder/HostWorldImpl.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace wit_host;
-
-public class HostWorldImpl : HostWorld
-{
-    public static void Run()
-    {
-        exports.HostWorld.Print("Hello from .NET");
-    }
-}
---
+++ samples/calculator/Adder/OperationsImpl.cs
@@ -0,0 +1,9 @@
+﻿namespace wit_computer.Wit.exports.example.calculator.Operations;
+
+public class OperationsImpl : Operations
+{
+    public static int Add(int left, int right)
+    {
+        return left + right;
+    }
+}
