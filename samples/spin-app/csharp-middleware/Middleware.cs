df519eab06d0515bf531511515968e03a6dd1023
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2023-12-01 12:07:30 +0000

    Begin adding .NET middleware

---
+++ samples/spin-app/csharp-middleware/Middleware.cs
@@ -0,0 +1,8 @@
+﻿namespace wit_computer.Wit.exports.example.calculator.Operations;
+
+public class OperationsImpl : Operations
+{
+    public static int Add(int left, int right) => left+right-1;
+    
+    //public static string ToUpper(string input) => input.ToUpperInvariant();
+}
--- samples/spin-app/rust-host/build.cmd
+++ samples/spin-app/rust-host/build.cmd
@@ -1,4 +1,8 @@
 cargo component build
 cargo build --target wasm32-wasi --release
 ..\..\..\src\WasmComponent.Sdk\tools\win-x64\wasm-tools component new target/wasm32-wasi/release/my_rust_app.wasm --adapt ..\..\..\src\WasmComponent.Sdk\tools\wasi-wasm\wasi_snapshot_preview1.reactor.wasm -o target/wasm32-wasi/release/my_rust_app.component.wasm
-..\..\..\src\WasmComponent.Sdk\tools\win-x64\wasm-tools compose -o target\wasm32-wasi\release\my_rust_app.composed.wasm target\wasm32-wasi\release\my_rust_app.component.wasm -d ..\c-middleware\dist\main.component.wasm
+
+set MIDDLEWARE_COMPONENT=..\c-middleware\dist\main.component.wasm
+set MIDDLEWARE_COMPONENT=..\csharp-middleware\bin\Debug\net8.0\wasi-wasm\native\csharp-middleware.component.wasm
+
+..\..\..\src\WasmComponent.Sdk\tools\win-x64\wasm-tools compose -o target\wasm32-wasi\release\my_rust_app.composed.wasm target\wasm32-wasi\release\my_rust_app.component.wasm -d %MIDDLEWARE_COMPONENT%
